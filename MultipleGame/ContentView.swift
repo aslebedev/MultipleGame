9da87a853f12a89e84fec63737e2c98902efae14
Alexander Lebedev <alexander.s.lebedev@gmail.com>
2019-10-28 22:06:36 +0700

    Initial Commit

---
+++ MultipleGame/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MultipleGame
+//
+//  Created by alexander on 28.10.2019.
+//  Copyright © 2019 alexander. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
